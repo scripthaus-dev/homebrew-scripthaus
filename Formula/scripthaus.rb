fa15275f2b6a16639f2f4af34f70c54bedc11447
sawka <mike.sawka@gmail.com>
2022-05-21 12:27:29 -0700

    scripthaus 0.1.0 (new formula)
    * scripthaus v0.1.0 (new formula)
    * update description
    * another update to desc to match homebrew style
    * fix the go build command
    * add bin.install
    * debugging
    * update bin name
    
    Closes #1.
    
    Signed-off-by: sawka <2722291+sawka@users.noreply.github.com>

---
+++ Formula/scripthaus.rb
@@ -0,0 +1,18 @@
+class Scripthaus < Formula
+  desc "Command-line tool that helps organize your scripts and bash one-liners"
+  homepage "https://www.scripthaus.dev"
+  url "https://github.com/scripthaus-dev/scripthaus/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "12e715dc23c908ffb66059fd1bdd9ace60cdcddbefb832a9d13fbdf1240e90bc"
+  license "MPL-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-trimpath", "-o", "scripthaus", "cmd/main.go"
+    bin.install "scripthaus"
+  end
+
+  test do
+    assert_match "^ScriptHaus v0.1.0", shell_output("#{bin}/scripthaus version")
+  end
+end
