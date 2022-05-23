3da84fdaeb47930a4e4a22e891ae3d629dd1ce7b
sawka <2722291+sawka@users.noreply.github.com>
2022-05-23 23:07:51 +0000

    scripthaus: add 0.2.1 bottle.

--- Formula/scripthaus.rb
+++ Formula/scripthaus.rb
@@ -5,6 +5,12 @@ class Scripthaus < Formula
   sha256 "93c423f38100bf6f762b1efed4ce7cb31a965d01f9f25d413276a2b86581e5fc"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/scripthaus-dev/homebrew-scripthaus/releases/download/scripthaus-0.2.1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "77edcfd39043f39dde40f885e5c5470227665fafee43def48970ceb2aabfcd9a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6288a55ac68a77e76cbbad7067306014f071c20a22406a4ccbf6b2fd6b12c7a0"
+  end
+
   depends_on "go" => :build
 
   def install
