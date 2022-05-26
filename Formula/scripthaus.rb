66af97fd5246f44509d3cd960fddacf302d1808f
sawka <mike.sawka@gmail.com>
2022-05-25 22:12:27 -0700

    scripthaus v0.3.1

--- Formula/scripthaus.rb
+++ Formula/scripthaus.rb
@@ -1,15 +1,9 @@
 class Scripthaus < Formula
   desc "Command-line tool that helps organize your scripts and bash one-liners"
   homepage "https://www.scripthaus.dev"
-  url "https://github.com/scripthaus-dev/scripthaus/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "0d057dd7f359c2124c23e4b07bd6c6cb49876fea83c1576e5f8b6b7f6580c561"
+  url "https://github.com/scripthaus-dev/scripthaus/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "5a07b34e5a3cee8f8c4c34affcd35809edd208a0edc998f142df3e4b8139213a"
   license "MPL-2.0"
-
-  bottle do
-    root_url "https://github.com/scripthaus-dev/homebrew-scripthaus/releases/download/scripthaus-0.3.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "d8c8987dcbb060aa2b9ce50dab1abe05ea7716e8384acf0ba4cf83f614351df9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "64529a81b529685192cc1e869acd35bd035f37c940a9eca2f47795bdfaf6e412"
-  end
 
   depends_on "go" => :build
 
