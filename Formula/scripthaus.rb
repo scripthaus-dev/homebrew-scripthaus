b3836425c5b1967f31823fc4777c98c82d67aead
sawka <mike.sawka@gmail.com>
2022-06-01 00:48:26 -0700

    scripthaus 0.5.0
    update for v0.5.0
    
    Closes #8.
    
    Signed-off-by: sawka <2722291+sawka@users.noreply.github.com>

--- Formula/scripthaus.rb
+++ Formula/scripthaus.rb
@@ -1,15 +1,9 @@
 class Scripthaus < Formula
   desc "Command-line tool that helps organize your scripts and bash one-liners"
   homepage "https://www.scripthaus.dev"
-  url "https://github.com/scripthaus-dev/scripthaus/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "619d54fd207aeba535ceccc289f49b65283648234b57b0e872794d1f229523b8"
+  url "https://github.com/scripthaus-dev/scripthaus/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "d7f8120a7f629fad6959769d540f91218ebdc13595da9edb5d87f0d4bfcba73a"
   license "MPL-2.0"
-
-  bottle do
-    root_url "https://github.com/scripthaus-dev/homebrew-scripthaus/releases/download/scripthaus-0.4.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "b3aa51eb45e5f0dbe949b160f0dc3d20d0a0ef8a11cd01f70b0c7312ead6354f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "60a20a1254625de0b02f1222d22a6f5f7f836b20cdf33bb4f9e6e170024ce20f"
-  end
 
   depends_on "go" => :build
 
@@ -33,6 +27,6 @@ class Scripthaus < Formula
   end
 
   test do
-    assert_match "[^scripthaus] v0.4.0", shell_output("#{bin}/scripthaus version")
+    assert_match "[^scripthaus] v0.5.0", shell_output("#{bin}/scripthaus version")
   end
 end
